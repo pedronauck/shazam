9289a23c0d1d579145eb7fcd165427c89fdc3e5f
Pedro Nauck <pedronauck@gmail.com>
2016-08-12 19:19:54 -0300

    Muda templates default do proejto

--- template/app/components/Root.js
+++ template/app/components/Root.js
@@ -1,32 +1,14 @@
 import React, { PropTypes } from 'react';
-import { Router } from 'react-router';
+import { Router, browserHistory } from 'react-router';
 
 import routes from '../routes';
 
-if (process.env.NODE_ENV === 'dev') {
-  Router.prototype.componentWillReceiveProps = (nextProps) => {
-    const components = [];
-    const grabComponents = (routesProp) => {
-      routesProp.props.children.forEach((route) => {
-        if (route.component) {
-          components.push(route.component)
-        }
-        if (route.indexRoute && route.indexRoute.component) {
-          components.push(route.indexRoute.component)
-        }
-        if (route.childRoutes) {
-          grabComponents(route.childRoutes)
-        }
-      });
-    };
-
-    grabComponents(nextProps.routes);
-    components.forEach(React.createElement);
-  }
-}
-
-const Root = () => (
-  <Router routes={routes} />
+const Root = (history) => (
+  <Router history={browserHistory} routes={routes} />
 );
+
+Root.PropTypes = {
+  history: PropTypes.object
+};
 
 export default Root;
--- template/app/main.js
+++ template/app/main.js
@@ -3,31 +3,19 @@ import 'stylesheets/main';
 import React from 'react';
 import Perf from 'react-addons-perf';
 import { render } from 'react-dom';
-import { AppContainer } from 'react-hot-loader';
-import { browserHistory } from 'react-router';
-
 import Root from './components/Root';
 
 const rootEl = document.getElementById('root');
-
 const renderApp = (RootComponent) => {
   render(
-    <AppContainer>
-      <RootComponent />
-    </AppContainer>,
+    <RootComponent />,
     rootEl
   );
 };
 
-if (process.env.NODE_ENV === 'dev') {
+if (process.env.NODE_ENV === 'development') {
   window.Perf = Perf;
   renderApp(Root);
-}
-
-if (process.env.NODE_ENV === 'dev' && module.hot) {
-  module.hot.accept('./components/Root', () => {
-    renderApp(require('./components/Root').default);
-  });
 }
 
 if (process.env.NODE_ENV === 'production') {
