174b327fb36ae067accb0a86553aad18dff36e9e
Pedro Nauck <pedronauck@gmail.com>
2016-08-12 19:19:30 -0300

    Muda arquivo de bin para usar yargs

--- bin/shazam.js
+++ bin/shazam.js
@@ -1,4 +1,18 @@
 #!/usr/bin/env node
+/* eslint no-unused-expressions: 0, func-names: 0 */
 
-require('babel-register');
-require('../lib');
+const yargs = require('yargs');
+
+yargs
+  .usage('$0 <cmd> [args]')
+  .command('init [name]', 'create app structure', {}, function(argv) {
+    require('../scripts/init')(argv.name);
+  })
+  .command('start', 'run development server', {}, function(argv) {
+    require('../scripts/start')();
+  })
+  .command('build', 'build production version of app', {}, function(argv) {
+    require('../scripts/build')();
+  })
+  .help()
+  .argv;
