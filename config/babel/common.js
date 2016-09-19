77cb417e8a95439d160fccebb2a94baa9c19f822
Pedro Nauck <pedronauck@gmail.com>
2016-09-19 17:35:24 -0300

    Ajusta configs do babel

--- config/babel/common.js
+++ config/babel/common.js
@@ -10,8 +10,6 @@ module.exports = {
     require.resolve('babel-plugin-syntax-trailing-function-commas'),
     require.resolve('babel-plugin-transform-class-properties'),
     require.resolve('babel-plugin-transform-object-rest-spread'),
-    require.resolve('babel-plugin-transform-react-pure-class-to-function'),
-    require.resolve('babel-plugin-transform-react-remove-prop-types'),
     require.resolve('babel-plugin-transform-export-extensions'),
     [require.resolve('babel-plugin-transform-runtime'), {
       helpers: false,
--- config/babel/production.js
+++ config/babel/production.js
@@ -1,5 +1,9 @@
 const common = require('./common');
 
-common.plugins.push(require.resolve('babel-plugin-transform-react-constant-elements'));
+common.plugins.push(
+  require.resolve('babel-plugin-transform-react-constant-elements'),
+  require.resolve('babel-plugin-transform-react-remove-prop-types'),
+  require.resolve('babel-plugin-transform-react-pure-class-to-function')
+);
 
 module.exporst = common;
