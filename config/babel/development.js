ea7aa8a870ee7f237ffd1c8e8356f9d599e9467f
Pedro Nauck <pedronauck@gmail.com>
2016-09-20 17:16:59 -0300

    Ajusta configs do babel

--- config/babel/development.js
+++ config/babel/development.js
@@ -1,5 +1,9 @@
 const common = require('./common');
 
 common.plugins.unshift(require.resolve('react-hot-loader/babel'));
+common.plugins.push(
+  require.resolve('babel-plugin-transform-react-remove-prop-types'),
+  require.resolve('babel-plugin-transform-react-pure-class-to-function')
+);
 
 module.exports = common;
--- config/babel/production.js
+++ config/babel/production.js
@@ -1,9 +1,7 @@
 const common = require('./common');
 
 common.plugins.push(
-  require.resolve('babel-plugin-transform-react-constant-elements'),
-  require.resolve('babel-plugin-transform-react-remove-prop-types'),
-  require.resolve('babel-plugin-transform-react-pure-class-to-function')
+  require.resolve('babel-plugin-transform-react-constant-elements')
 );
 
 module.exporst = common;
