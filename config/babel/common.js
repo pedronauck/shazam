00d133052f1a262eaea3114a5392ad988d9953e8
Pedro Nauck <pedronauck@gmail.com>
2017-01-10 23:56:29 -0200

    redefine babel preset with new packages

--- config/babel/common.js
+++ config/babel/common.js
@@ -1,19 +1,27 @@
+const path = require('path');
+
 module.exports = {
   babelrc: false,
   cacheDirectory: true,
   presets: [
-    require.resolve('babel-preset-latest'),
-    require.resolve('babel-preset-stage-1'),
+    [require.resolve('babel-preset-latest'), { modules: false }],
     require.resolve('babel-preset-react')
   ],
   plugins: [
     require.resolve('babel-plugin-syntax-trailing-function-commas'),
     require.resolve('babel-plugin-transform-class-properties'),
-    require.resolve('babel-plugin-transform-object-rest-spread'),
     require.resolve('babel-plugin-transform-export-extensions'),
+    [require.resolve('babel-plugin-transform-object-rest-spread'), {
+      useBuiltIns: true
+    }],
+    [require.resolve('babel-plugin-transform-react-jsx'), {
+      useBuiltIns: true
+    }],
     [require.resolve('babel-plugin-transform-runtime'), {
       helpers: false,
-      polyfill: false
+      polyfill: false,
+      regenerator: true,
+      moduleName: path.dirname(require.resolve('babel-runtime/package'))
     }]
   ]
 };
--- config/babel/development.js
+++ config/babel/development.js
@@ -1,15 +1,10 @@
 const common = require('./common');
-const argv = require('yargs').argv;
-
-const hasHotLoader = argv.hotLoader;
-
-if (hasHotLoader) {
-  common.plugins.unshift(require.resolve('react-hot-loader/babel'));
-}
 
 common.plugins.push(
   require.resolve('babel-plugin-transform-react-remove-prop-types'),
-  require.resolve('babel-plugin-transform-react-pure-class-to-function')
+  require.resolve('babel-plugin-transform-react-pure-class-to-function'),
+  require.resolve('babel-plugin-transform-react-jsx-source'),
+  require.resolve('babel-plugin-transform-react-jsx-self')
 );
 
 module.exports = common;
