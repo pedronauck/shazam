80370a75e08a8e662083045a08cfd16ec0f056cb
Pedro Nauck <pedronauck@gmail.com>
2017-02-03 17:57:04 -0200

    fix: set module.hot configuration to template default

--- template/default/main.js
+++ template/default/main.js
@@ -1,8 +1,23 @@
 import 'stylesheets/base';
 
 import React from 'react';
+import { AppContainer } from 'react-hot-loader';
 import { render } from 'react-dom';
 
 import App from 'components/App';
 
-render(<App />, document.getElementById('root'));
+const rootEl = document.getElementById('root');
+const renderApp = (Comp = App) => {
+  render((
+    <AppContainer>
+      <Comp />
+    </AppContainer>
+  ), rootEl);
+};
+
+if (module.hot) {
+  module.hot.accept('./components/App', () =>
+    renderApp(require('./components/App').default));
+}
+
+renderApp();
